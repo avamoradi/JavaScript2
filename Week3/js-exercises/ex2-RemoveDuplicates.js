125194f5adc633d788f504616916ee7a86fe8d13
Ava Moradi <ava.moradi86@gmail.com>
2020-08-17 13:18:20 +0200

    ex2-removeduplicated.js file added

--- Week3/js-exercises/ex2-RemoveDuplicates.js
+++ Week3/js-exercises/ex2-RemoveDuplicates.js
@@ -12,10 +12,31 @@ does not return anything but removes any duplicate elements from the array.
 
 
 // WRITE YOUR FUNCTION HERE
+function removeDuplicates(arr) {
+    arr.forEach(element => {
+    check(element)
+  });
+  function check(x) {
+    let counter = 0 ;
+    for (let i = 0; i<arr.length; i++){
+      if (x === arr[i]) {
+         counter ++;
+        if (counter > 1 ) {
+          arr.splice(i, 1);
+        }
+      }
+    }
+  }
+  return arr
+}
 
-const letters = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c', 'b'];
 
-removeDuplicates(letter);
+ const letters = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c', 'b'];
 
-if (letters === ['a', 'b', 'c', 'd', 'e', 'f'])
-  console.log("Hooray!")
+ const newArray = removeDuplicates(letters);
+
+ if (JSON.stringify(newArray)  === JSON.stringify(['a', 'b', 'c', 'd', 'e', 'f'])) 
+
+ console.log("Hooray!")
+ 
+
