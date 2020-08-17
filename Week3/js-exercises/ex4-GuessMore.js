ec33a66bc4c31dfc02fa3a530b71d8d76867c300
Ava Moradi <ava.moradi86@gmail.com>
2020-08-17 14:49:38 +0200

    ex4-GuessMore.js file added

--- Week3/js-exercises/ex4-GuessMore.js
+++ Week3/js-exercises/ex4-GuessMore.js
@@ -11,11 +11,16 @@ Write out your reasoning in 50 words or less.
 const x = 9;
 
 function f1(val) {
-  val = val + 1;
-  return val;
+  val = val + 1; 
+  return val; //10
 }
-f1(x);
-console.log(x);
+f1(x);//10
+console.log(x);//9
+
+/** console.log(x) would print 9 in the console ,
+ we call f1(9) then inside the function 9 + 1 = 10
+ so in return f1(10), but we didnt assigned the return of function to any variable , so x is just 10 inside the function but outside the function is 9
+  */
 
 const y = {
   x: 9
@@ -27,3 +32,8 @@ function f2(val) {
 }
 f2(y);
 console.log(y);
+console.log(x);
+
+/** console.log(y) would print {x:10}
+ because y is an objest and x is property of this object, and its not a defined variable .
+ */
