8c4359728a76e817055e4d195187ef94aae7c5fd
Ava Moradi <ava.moradi86@gmail.com>
2020-08-17 14:18:50 +0200

    ex3-GuessTheOutput.js added

--- Week3/js-exercises/ex3-GuessTheOutput.js
+++ Week3/js-exercises/ex3-GuessTheOutput.js
@@ -7,7 +7,7 @@ Can you guess the output?
 Write out your reasoning in 50 words or less.
 
 */
-
+// 
 
 
 let a = 10;
@@ -19,3 +19,8 @@ const x = (function () {
 })();
 
 x();
+
+/* resault of this code is an alert message which shows 12
+the a is global variable and let which means that could be overwrite . 
+at the first a = 10, when we call function x(), a = 12 , then we have a closure  , the inner function could be access the outer function variables so here is also a = 12 , and by line alert(a), we going to alert(12)
+*/ 
